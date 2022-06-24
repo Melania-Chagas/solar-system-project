23b77008fc9b1d698e25b5f1e8d8b3d6afe1aea9
Melania P Chagas <melaniapchagas@gmail.com>
2022-06-24 12:49:57 -0300

    Requisito 7: Criando componente 'Missions'

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        teste;
+      </div>
+    );
+  }
+}
+
+export default Missions;
