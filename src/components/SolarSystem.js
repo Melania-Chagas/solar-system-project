a60c8f6c93f00ae886814fc8a6cbb3a4eedf5026
Melania P Chagas <melaniapchagas@gmail.com>
2022-06-24 12:04:44 -0300

    Requisito 5: criando componente 'PlanetCard'

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        test;
         <Title headline="Planetas" />
+        <section>
+          {
+            Planets.forEach((planet) => (<PlanetCard
+              key={ planet.name }
+              planetName={ planet.name }
+              planetImage={ planet.image }
+            />))
+          }
+        </section>
       </div>
     );
   }
